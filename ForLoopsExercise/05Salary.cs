7c9ab8ec017639a5db10fba91dc44158d2b25a45
dianGrigorov <diangrigorov@gmail.com>
2022-12-28 01:10:39 +0200

    Create 05Salary.cs

---
+++ ForLoopsExercise/05Salary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+
+namespace _05Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < tabs; i++)
+            {
+                string website = Console.ReadLine();
+                if (website == "Facebook")
+                {
+                    salary -= 150;
+                }
+                else if (website == "Instagram")
+                {
+                    salary -= 100;
+                }
+                else if (website == "Reddit")
+                {
+                    salary -= 50;
+                }
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    return;
+                }
+            }
+            if (salary > 0)
+            {
+                Console.WriteLine(salary);
+            }
+
+        }
+    }
+}
