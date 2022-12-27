ff8bf02b509e69e29dca641921ccda755ce7e334
dianGrigorov <diangrigorov@gmail.com>
2022-12-27 21:00:47 +0200

    Create 08.NumberSequence.cs

---
+++ ForLoops/08.NumberSequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08.NumberSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (max < number)
+                {
+                    max = number;
+                }
+                if (min > number)
+                {
+                    min = number;
+                }
+            }
+            Console.WriteLine($"Max number: {max}");
+            Console.WriteLine($"Min number: {min}");
+        }
+    }
+}
