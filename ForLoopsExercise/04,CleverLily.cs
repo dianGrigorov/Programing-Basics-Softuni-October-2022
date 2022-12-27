709b0076235a49cf0c83ac00b467c275d4556717
dianGrigorov <diangrigorov@gmail.com>
2022-12-28 01:10:37 +0200

    Create 04,CleverLily.cs

---
+++ ForLoopsExercise/04,CleverLily.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04_CleverLily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washingMachine = double.Parse(Console.ReadLine());
+            int toyPrise = int.Parse(Console.ReadLine());
+            int money = 0;
+            int moneyUpdate = 0;
+            int toys = 0;
+            for (int i =1 ; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    money += i / 2 * 10;
+                    money--;
+                   
+                }
+                else
+                {
+                    toys++;
+                }
+            }
+            toyPrise *= toys;
+            double totalMoney = money + toyPrise;
+            if (totalMoney >= washingMachine)
+            {
+                Console.WriteLine($"Yes! {totalMoney - washingMachine:f2}");
+            }
+            else
+            {
+                double diff = Math.Abs(totalMoney - washingMachine);
+                Console.WriteLine($"No! {diff:f2}");
+            }
+
+        }
+    }
+}
