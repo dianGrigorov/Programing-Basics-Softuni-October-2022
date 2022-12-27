05d147a1287200194d5caf377f691fece50ca83f
dianGrigorov <diangrigorov@gmail.com>
2022-12-28 01:10:46 +0200

    Create 08.TenisRanklist.cs

---
+++ ForLoopsExercise/08.TenisRanklist.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _08.TenisRanklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numTour = int.Parse(Console.ReadLine());
+            int startPoints = int.Parse(Console.ReadLine());
+            int totalPoints = 0;
+            int winTour = 0;
+            for (int i = 1; i <= numTour; i++)
+            {
+                string stage = Console.ReadLine();
+                if (stage == "W")
+                {
+                    totalPoints += 2000;
+                    winTour++;
+                }
+                else if (stage == "F")
+                {
+                    totalPoints += 1200;
+                }
+                else if (stage == "SF")
+                {
+                    totalPoints += 720;
+                }
+            }
+            double winPercent = ((double)winTour / numTour) * 100;
+            double averagePoints = Math.Floor((double)totalPoints / numTour);
+            Console.WriteLine($"Final points: {totalPoints + startPoints}");
+            Console.WriteLine($"Average points: {averagePoints}");
+            Console.WriteLine($"{winPercent:f2}%");
+        }
+    }
+}
