b09fccf57d4df02ef7042b6ea4e99220a54e8228
dianGrigorov <diangrigorov@gmail.com>
2022-12-27 21:00:50 +0200

    Create 10.OddEvenSum.cs

---
+++ ForLoops/10.OddEvenSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _10.OddEvenSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int even = 0;
+            int odd = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    even += num;
+                }
+                else
+                {
+                    odd += num;
+                }
+                
+            }
+            if (even == odd)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {even}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                int diff = Math.Abs(even - odd);
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
