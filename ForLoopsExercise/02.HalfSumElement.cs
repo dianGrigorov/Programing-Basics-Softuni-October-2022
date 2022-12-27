bc66a492b93d1243f8954de3936a58cfd6cee7b4
dianGrigorov <diangrigorov@gmail.com>
2022-12-28 01:10:32 +0200

    Create 02.HalfSumElement.cs

---
+++ ForLoopsExercise/02.HalfSumElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02.HalfSumElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int total = 0;
+            int max = int.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                total += num;
+                if (num > max)
+                {
+                    max = num;
+                }
+
+            }
+            int sumWithoutMax = total - max;
+            if (sumWithoutMax == max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sumWithoutMax}");
+            }
+            else
+            {
+                int diff = Math.Abs(max - sumWithoutMax);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
