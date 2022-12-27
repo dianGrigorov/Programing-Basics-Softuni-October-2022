ed19b60fcd89aafc5ee806cdda9e4efc0608c2f9
dianGrigorov <diangrigorov@gmail.com>
2022-12-27 21:00:23 +0200

    Create 02.NumFromNTo1.cs

---
+++ ForLoops/02.NumFromNTo1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02.NumFromNTo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = num ; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
