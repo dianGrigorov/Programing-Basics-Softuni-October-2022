ed31d448fe93dc6f515b018a7de3b9e577ac47f8
dianGrigorov <diangrigorov@gmail.com>
2022-12-28 01:10:41 +0200

    Create 06.Oscars.cs

---
+++ ForLoopsExercise/06.Oscars.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06Oscars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string actоr = Console.ReadLine();
+            double points = double.Parse(Console.ReadLine());
+            int refers = int.Parse(Console.ReadLine());
+           
+            
+            for (int i = 0; i < refers; i++)
+            {
+                string referName = Console.ReadLine();
+                double refPoints = double.Parse(Console.ReadLine());
+
+                points += referName.Length * refPoints / 2 ;
+                if (points > 1250.5)
+                {
+                    Console.WriteLine($"Congratulations, {actоr} got a nominee for leading role with {points:f1}!");
+                    return;
+                }
+            }
+            Console.WriteLine($"Sorry, {actоr} you need {1250.5 - points:f1} more!");
+
+        }
+    }
+}
