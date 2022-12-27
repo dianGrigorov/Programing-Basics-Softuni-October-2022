21b9638ba9a43a663a6e0f22ffdba0ca1824ed47
dianGrigorov <diangrigorov@gmail.com>
2022-12-27 21:00:48 +0200

    Create 09.LeftAndRightSum.cs

---
+++ ForLoops/09.LeftAndRightSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace _09.LeftAndRightSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int left = 0;
+            int right = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                left += num;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+               right += num;
+            }
+            if (left == right)
+            {
+                Console.WriteLine($"Yes, sum = {left}");
+            }
+            else
+            {
+                int diference = Math.Abs(left - right);
+                Console.WriteLine($"No, diff = {diference}");
+            }
+        }
+    }
+}
