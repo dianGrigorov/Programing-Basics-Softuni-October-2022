8ce9b322bc43f684a0fbd6b2c3a0dcec5e22f9e0
dianGrigorov <diangrigorov@gmail.com>
2022-12-29 09:52:57 +0200

    Create 10.ClockPartTwo.cs

---
+++ ForLoopsMOreExercises/10.ClockPartTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10.ClockPartTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                for (int j = 0; j < 60; j++)
+                {
+                    for (int g = 0; g < 60; g++)
+                    {
+                        Console.Write(i);
+                        Console.Write(" : ");
+                        Console.Write(j);
+                        Console.Write(" : ");
+                        Console.WriteLine(g);
+                    }
+                }
+            }
+        }
+    }
+}
