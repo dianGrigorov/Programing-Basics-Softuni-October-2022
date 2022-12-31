b50b7015d3cb6d82c484561a61369d768324b1c4
dianGrigorov <diangrigorov@gmail.com>
2022-12-31 10:32:57 +0200

    Create 01.OldBook.cs

---
+++ WhileLoopsExercise/01.OldBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WhileLoopExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string book = Console.ReadLine();
+            string curentBook = Console.ReadLine();
+            int cnt = 0;
+            while (curentBook != "No More Books")
+            {
+                if (curentBook == book)
+                {
+                    Console.WriteLine($"You checked {cnt} books and found it.");
+                    return;
+                }
+                cnt++;
+                curentBook = Console.ReadLine();
+
+            }
+            Console.WriteLine($"The book you search is not here!");
+            Console.WriteLine($"You checked {cnt} books.");
+        }
+    }
+}
