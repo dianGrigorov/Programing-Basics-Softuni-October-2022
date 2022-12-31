d04253a8dde7130fd688f32f97cdb002c08a0ac5
dianGrigorov <diangrigorov@gmail.com>
2022-12-31 10:08:10 +0200

    Create 05.Travelling.cs

---
+++ NestedLoops/05.Travelling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05.Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination;
+            
+
+            while ((destination = Console.ReadLine()) != "End")
+            {
+                double tripPrice = double.Parse(Console.ReadLine());
+                double total = 0;
+
+                while (total < tripPrice)
+                {
+                    double savedMoney = double.Parse(Console.ReadLine());
+                    total += savedMoney;
+
+                }
+                Console.WriteLine($"Going to {destination}!");
+               
+               
+            }
+        }
+    }
+}
