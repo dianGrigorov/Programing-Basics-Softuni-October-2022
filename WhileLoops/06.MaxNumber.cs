1fb1b2b5d3d22fc837a21d18b3cc8833a664fb71
dianGrigorov <diangrigorov@gmail.com>
2022-12-31 10:29:48 +0200

    Create 06.MaxNumber.cs

---
+++ WhileLoops/06.MaxNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06.MaxNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            int maxNum = int.MinValue;
+            while (num != "Stop")
+            {
+                int curentNum = int.Parse(num);
+                if (maxNum < curentNum)
+                {
+                    maxNum = curentNum;
+                }
+                num = Console.ReadLine();
+            }
+            Console.WriteLine(maxNum);
+        }
+    }
+}
