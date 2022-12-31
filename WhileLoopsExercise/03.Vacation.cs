35fda7cf5306227da05bf6e2476ebc4810c2c1e3
dianGrigorov <diangrigorov@gmail.com>
2022-12-31 10:33:00 +0200

    Create 03.Vacation.cs

---
+++ WhileLoopsExercise/03.Vacation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _03.Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double vacationPrice = double.Parse(Console.ReadLine());
+            double totalMoney = double.Parse(Console.ReadLine());
+            int cntDays = 0;
+            int totalDays = 0;
+            while (vacationPrice > totalMoney)
+            {
+                string action = Console.ReadLine();
+                double money = double.Parse(Console.ReadLine());
+                totalDays++;
+                if (action == "spend")
+                {
+                    cntDays++;
+                    totalMoney -= money;
+                    if (totalMoney <0)
+                    {
+                        totalMoney = 0;
+                    }
+                    if (cntDays >= 5)
+                    {
+                        Console.WriteLine("You can't save the money.");
+                        Console.WriteLine($"{totalDays}");
+                        return;
+                    }
+                }
+                if (action == "save")
+                {
+                    totalMoney += money;
+                    cntDays = 0;
+                }
+            }
+            Console.WriteLine($"You saved the money for {totalDays} days.");
+        }
+    }
+}
