f4201cb317d13324a8156bc32baca9bebaa8c445
dianGrigorov <diangrigorov@gmail.com>
2022-12-31 10:33:06 +0200

    Create 07.Moving.cs

---
+++ WhileLoopsExercise/07.Moving.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _07.Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int hight = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int area = lenght * hight *width;
+            string comand = Console.ReadLine();
+            int totalBoxes = 0;
+            while (comand != "Done")
+            {
+                int boxes = int.Parse(comand);
+                totalBoxes += boxes;
+                if (area < totalBoxes)
+                {
+                    Console.WriteLine($"No more free space! You need {totalBoxes - area} Cubic meters more.");
+                    return;
+                }
+                comand = Console.ReadLine();
+
+            }
+            if (area >= totalBoxes)
+            {
+                Console.WriteLine($"{Math.Abs(area - totalBoxes)} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {area - totalBoxes} Cubic meters more.");
+            }
+        }
+    }
+}
