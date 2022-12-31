3a5a93c283c38e3ae8eb181655ebe5f1e60cced2
dianGrigorov <diangrigorov@gmail.com>
2022-12-31 10:10:56 +0200

    Create 01.NumberPyramid.cs

---
+++ NestedLoopsExercise/01.NumberPyramid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01.NumberPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int cnt = 1;
+            bool isBiger = false;
+            for (int rows = 1; rows <= num; rows++)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    
+                    if (cnt > num)
+                    {
+                        isBiger = true;
+                        break;
+                    }
+                    Console.Write(cnt + " ");
+                    cnt++;  
+                }
+                if (isBiger)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
