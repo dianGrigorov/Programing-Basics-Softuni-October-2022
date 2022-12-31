dfc508f4880134d7864b9221fb607269c00b0602
dianGrigorov <diangrigorov@gmail.com>
2022-12-31 10:29:39 +0200

    Create 02Password.cs

---
+++ WhileLoops/02Password.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = Console.ReadLine();
+            string passwordUser = Console.ReadLine();
+            string input = Console.ReadLine();
+            while (input != passwordUser )
+            {
+               input = Console.ReadLine();
+            }
+            Console.WriteLine($"Welcome {userName}");
+        }
+    }
+}
