cec59b7a2cc37a35bef5c5a0d4cf91b93a17ca55
dianGrigorov <diangrigorov@gmail.com>
2022-12-31 10:08:08 +0200

    Create 04.SumOfTwoNumbers.cs

---
+++ NestedLoops/04.SumOfTwoNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04.SumOfTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+            int count = 0;
+            bool isTrue = false;
+
+            for (int i = n1; i <= n2; i++)
+            {
+                for (int j = n1; j <= +n2; j++)
+                {
+                    count++;
+                    if (i + j == magicNum)
+                    {
+                        Console.WriteLine($"Combination N:{count} ({i} + {j} = {magicNum})");
+                        isTrue = true;
+                        break;
+                    }
+
+                }
+                if (isTrue)
+                {
+                    break;
+                }
+            }
+
+            if (!isTrue)
+            {
+                Console.WriteLine($"{count} combinations - neither equals {magicNum}");
+            }
+        }
+    }
+}
