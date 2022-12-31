88780a33e3843a8f461ff3d8e466a907c89dc9f2
dianGrigorov <diangrigorov@gmail.com>
2022-12-31 10:11:04 +0200

    Create 04.TrainTheTrainers.cs

---
+++ NestedLoopsExercise/04.TrainTheTrainers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04.TrainTheTrainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int judges = int.Parse(Console.ReadLine());
+            string presentation;
+            int presentationCnt = 0;
+            double averageSuccess = 0;
+            while ((presentation = Console.ReadLine()) != "Finish")
+            {
+                presentationCnt++;
+                double totalEvaluation = 0;
+                for (int i = 0; i < judges; i++)
+                {
+                    double evaluation = double.Parse(Console.ReadLine());
+                    totalEvaluation += evaluation;
+                }
+                double averageEvaluation = totalEvaluation / judges;
+                averageSuccess += averageEvaluation;
+                Console.WriteLine($"{presentation} - {averageEvaluation:f2}.");
+            }
+
+            Console.WriteLine($"Student's final assessment is {(averageSuccess / presentationCnt):f2}.");
+        }
+    }
+}
