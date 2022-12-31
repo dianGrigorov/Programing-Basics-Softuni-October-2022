31c7dec3baeef07e8def00e6fe12fabdb1733bc6
dianGrigorov <diangrigorov@gmail.com>
2022-12-31 10:29:44 +0200

    Create 04.Sequence2kPlus1.cs

---
+++ WhileLoops/04.Sequence2kPlus1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04.Sequence2kPlus1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int output = 1;
+            while (output <= input)
+            {
+                Console.WriteLine(output);
+                
+                output = output * 2 + 1;
+              
+
+            }
+        }
+    }
+}
