b86c431801671b2e6cc681247e1bd3823080adb6
dianGrigorov <diangrigorov@gmail.com>
2022-12-31 10:29:51 +0200

    Create 07.MinNumber.cs

---
+++ WhileLoops/07.MinNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06.MaxNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            int minNum = int.MaxValue;
+            while (num != "Stop")
+            {
+                int curentNum = int.Parse(num);
+                if (minNum > curentNum)
+                {
+                    minNum = curentNum;
+                }
+                num = Console.ReadLine();
+            }
+            Console.WriteLine(minNum);
+        }
+    }
+}
