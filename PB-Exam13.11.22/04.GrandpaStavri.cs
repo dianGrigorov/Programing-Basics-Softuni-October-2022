3438003c060f22ff089bc249d2a21fa20e3ca463
dianGrigorov <diangrigorov@gmail.com>
2023-01-01 03:30:44 +0200

    Create 04.GrandpaStavri.cs

---
+++ PB-Exam13.11.22/04.GrandpaStavri.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.GrandpaStavri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double totalShnaps = 0;
+            double totalDegreeses = 0;
+
+            for (int i = 0; i < days; i++)
+            {
+                double quantity = double.Parse(Console.ReadLine());
+                double degrees = double.Parse(Console.ReadLine());
+                totalShnaps += quantity;
+                totalDegreeses += degrees * quantity;
+            }
+            double averageDegreeses = totalDegreeses / totalShnaps;
+
+            Console.WriteLine($"Liter: {totalShnaps:f2}");
+            Console.WriteLine($"Degrees: {averageDegreeses:f2}");
+            if (averageDegreeses < 38)
+            {
+                Console.WriteLine("Not good, you should baking!");
+            }
+            else if (averageDegreeses >= 38 && averageDegreeses <= 42)
+            {
+                Console.WriteLine("Super!");
+            }
+            else if (averageDegreeses > 42)
+            {
+                Console.WriteLine($"Dilution with distilled water!");
+            }
+        }
+    }
+}
