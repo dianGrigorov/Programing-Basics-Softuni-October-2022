e4d67139a3bf76de91bd9e1ac579c6d9844761f4
dianGrigorov <diangrigorov@gmail.com>
2023-01-01 03:30:48 +0200

    Create 05.ExcursionSale.cs

---
+++ PB-Exam13.11.22/05.ExcursionSale.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _05.ExcursionSale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int seaPack = int.Parse(Console.ReadLine());
+            int mountainPack = int.Parse(Console.ReadLine());
+            int profit = 0;
+            string type;
+            bool noMore = false;
+            while ((type = Console.ReadLine()) != "Stop")
+            {
+                if (type == "sea")
+                {
+                    if (seaPack <= 0)
+                    {
+                        continue;
+                    }
+                    profit += 680;
+                    seaPack--;
+
+                }
+                else if (type == "mountain")
+                {
+                    if (mountainPack <= 0)
+                    {
+                        continue;
+                    }
+                    profit += 499;
+                    mountainPack--;
+
+                }
+                if (seaPack < 0 && mountainPack < 0)
+                {
+
+                }
+                if (seaPack <= 0 && mountainPack <= 0)
+                {
+                    break;
+                }
+            }
+            if (seaPack <= 0 && mountainPack <= 0)
+            {
+                Console.WriteLine(" Good job! Everything is sold.");
+            }
+            Console.WriteLine($"Profit: {profit} leva.");
+        }
+    }
+}
